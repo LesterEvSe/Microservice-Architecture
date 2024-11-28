d9d0c95ac828f79749b50b64067c5fff24985e33
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2024-11-28 21:30:18 +0200

    chore: clean some code

--- task-service/main.py
+++ task-service/main.py
@@ -1,15 +1,5 @@
-#from http.server import HTTPServer
 from service import RabbitMQClient
-
-#TASK_SERVICE=5002
 
 if __name__ == "__main__":
     client = RabbitMQClient('task_service_queue')
     client.start_consumer()
-    
-    '''
-    server_address = ('', TASK_SERVICE)
-    httpd = HTTPServer(server_address, TaskHandler)
-    print(f"Starting service on port {TASK_SERVICE}...")
-    httpd.serve_forever()
-    '''
